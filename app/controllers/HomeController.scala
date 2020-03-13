f2afd0746cd377a7749557856428f4581fc6f719
leeroyc710 <leeroychiweshe@academytrainee.com>
2020-03-13 15:25:54 +0000

    play framework

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,7 +7,9 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Your first new application is ready "))
   }
-
+  def help() = Action {
+    Ok("There is no help here...")
+  }
 }
