305b8a15cd3ca31d7f02ec243fc28a3c6c9e8d1e
leeroyc710 <leeroychiweshe@academytrainee.com>
2020-03-16 16:26:15 +0000

    play framework added login

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,14 +2,12 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import play.api.mvc.{AbstractController, ControllerComponents}
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents, authAction: AuthenticationAction) extends AbstractController(cc) {
 
-  def index = Action {
+  def index = Action[AnyContent] = authAction {
     Ok(views.html.index("Your first new application is ready "))
-  }
-  def help() = Action {
-    Ok("There is no help here...")
   }
 }
